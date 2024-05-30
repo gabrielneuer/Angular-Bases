d0922623d46ffb2ca261b09530b48825787ab297
Gabriel Arias Romero <garrishby10.gvgar@gmail.com>
2024-05-30 01:35:43 -0600

    aprendiendo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/hero.module';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
